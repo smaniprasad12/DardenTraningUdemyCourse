59706ce93bb8b73769ddfe7f83c021cc8eaa904f
siddpras <smaniprasad123@gmail.com>
2023-11-24 07:34:28 +0530

    UdemyCourse Handson

---
+++ springboot-restful-webservices-Dto/src/main/java/com/example/springboot/service/impl/UserServiceImpl.java
@@ -0,0 +1,63 @@
+package com.example.springboot.service.impl;
+
+import com.example.springboot.dto.UserDto;
+import com.example.springboot.entity.User;
+import com.example.springboot.mapper.UserMapper;
+import com.example.springboot.repository.UserRepository;
+import com.example.springboot.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+
+
+        User user = UserMapper.mapToUser(userDto);
+
+        User savedUser = userRepository.save(user);
+
+
+        UserDto savedUserDto = UserMapper.mapToUserDto(savedUser);
+
+        return savedUserDto;
+    }
+
+    @Override
+    public UserDto getUserById(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        User user = optionalUser.get();
+        return UserMapper.mapToUserDto(user);
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map(UserMapper::mapToUserDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDto updateUser(UserDto user) {
+        User existingUser = userRepository.findById(user.getId()).get();
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setEmail(user.getEmail());
+        User updatedUser = userRepository.save(existingUser);
+        return UserMapper.mapToUserDto(updatedUser);
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
